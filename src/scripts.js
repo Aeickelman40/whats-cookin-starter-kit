f00b1199ad02feae6cfe44a2f24aa556e5feefd2
Alex Eickelman <aeickelman40@gmail.com>
2020-04-01 14:23:17 -0600

    Fix bug with pantry search and alphabitze css properties

--- src/scripts.js
+++ src/scripts.js
@@ -15,11 +15,8 @@ const searchPantryButton = document.querySelector("#pantry-search-button");
 const randomUser = usersData[Math.floor(Math.random() * usersData.length)];
 const chosenUser = new User(randomUser);
 
-// headerButtonsContainer.addEventListener("click", filterMainPageRecipes);
 searchRecipesButton.addEventListener("click", searchRecipes);
 searchPantryButton.addEventListener("click", searchPantry);
-// searchPantryInput.addEventListener("input", searchPantry);
-// searchRecipesInput.addEventListener("input", searchRecipes);
 shoppingListButton.addEventListener("click", showShoppingList);
 recipesToCookButton.addEventListener("click", showRecipeQueue);
 favoriteRecipesButton.addEventListener("click", showFavoriteRecipes);
@@ -68,7 +65,7 @@ function searchRecipes() {
   var recipeCards = Array.from(document.querySelectorAll('.recipe-card'));
   for (var i = 0; i < recipeCards.length; i++) {
     var recipeName = recipeCards[i].querySelector('.recipe-name');
-    var recipeIngredient = recipeCards[i].querySelector('.recipe-info');
+    var recipeIngredient = recipeCards[i].querySelector('.ingredients-box');
     var recipeTag = recipeCards[i].querySelector('.recipe-tags');
     if (recipeName.innerText.includes(searchRecipesInput.value) ||
           recipeIngredient.innerText.includes(searchRecipesInput.value) ||
@@ -78,20 +75,13 @@ function searchRecipes() {
           recipeCards[i].classList.add('hidden');
         }
       }
-  //   if (!recipeName.innerText.includes(searchRecipesInput.value) ||
-  //     !recipeIngredient.innerText.includes(searchRecipesInput.value) ||
-  //     !recipeTag.innerText.includes(searchRecipesInput.value)) {
-  //     recipeCards[i].classList.add('hidden');
-  //   } else {
-  //     recipeCards[i].classList.remove('hidden');
-  //   }
-  // }
 }
 
 function searchPantry() {
-  var ingredientCards = Array.from(document.querySelectorAll('.ingedient-card'));
+  var ingredientCards = Array.from(document.querySelectorAll('.ingredient-card'));
+  console.log(ingredientCards.length);
   for (var i = 0; i < ingredientCards.length; i++) {
-    var ingredientName = ingredientCards[i].querySelector('.ingredient- name');
+    var ingredientName = ingredientCards[i].querySelector('.ingredient-name');
     if (!ingredientName.innerText.includes(searchPantryInput.value)) {
       ingredientCards[i].classList.add('hidden');
     } else {
