070c935002086131c72977d5517d8b2060fd917f
Alex Eickelman <aeickelman40@gmail.com>
2020-03-31 18:49:34 -0600

    Add search functionality for recipe and tag and began pantry search

--- src/scripts.js
+++ src/scripts.js
@@ -10,12 +10,16 @@ const searchRecipesInput = document.querySelector("#search-recipes-input");
 const ingredientsContainer = document.querySelector("#ingredients-container");
 const recipesContainer = document.querySelector("#recipes-container");
 const welcomeUserBanner = document.querySelector("#welcome-user-banner");
+const searchRecipesButton = document.querySelector("#search-recipes-button");
+const searchPantryButton = document.querySelector("#pantry-search-button");
 const randomUser = usersData[Math.floor(Math.random() * usersData.length)];
 const chosenUser = new User(randomUser);
 
 // headerButtonsContainer.addEventListener("click", filterMainPageRecipes);
-searchPantryInput.addEventListener("input", searchPantry);
-searchRecipesInput.addEventListener("input", searchRecipes);
+searchRecipesButton.addEventListener("click", searchRecipes);
+searchPantryButton.addEventListener("click", searchPantry);
+// searchPantryInput.addEventListener("input", searchPantry);
+// searchRecipesInput.addEventListener("input", searchRecipes);
 shoppingListButton.addEventListener("click", showShoppingList);
 recipesToCookButton.addEventListener("click", showRecipeQueue);
 favoriteRecipesButton.addEventListener("click", showFavoriteRecipes);
@@ -68,42 +72,42 @@ function showShoppingList() {
 
 }
 
-// function from checkyoself:
-// function filterBySearch() {
-//   var taskCards = Array.from(document.querySelectorAll('.task-card'));
-//   for (var i = 0; i < taskCards.length; i++) {
-//     var todoTitle = taskCards[i].querySelector('.card-title');
-//     if (!todoTitle.innerText.includes(searchInput.value)) {
-//       taskCards[i].classList.add('hide');
-//     } else {
-//       taskCards[i].classList.remove('hide');
-//     }
-//   }
-// }
-
-function searchPantry() {
-  // function that searches the pantry by ingredient name
-}
 
 function searchRecipes() {
-  // function that searches the recipes by
-  // ingredient, tag, or recipe name
-  // let newRecipe = new Recipe(recipe,
-  //   ingredientsData,
-  //   recipeData);
-  // return this.recipes.filter(recipe => {
-  //   let matchingIngredient;
-  //   return recipe.ingredients.find(ingredient => {
-  //     matchingIngredient = this.ingredients.find(specificIngredient => {
-  //       return specificIngredient.id === ingredient.id;
-  //     });
-  //     return (matchingIngredient.name.toLowerCase().includes(searchText)) ||
-  //       (recipe.name.toLowerCase().includes(searchText.toLowerCase()))
-  //   });
-  // })
+  var recipeCards = Array.from(document.querySelectorAll('.recipe-card'));
+  for (var i = 0; i < recipeCards.length; i++) {
+    var recipeName = recipeCards[i].querySelector('.recipe-name');
+    var recipeIngredient = recipeCards[i].querySelector('.recipe-info');
+    var recipeTag = recipeCards[i].querySelector('.recipe-tags');
+    if (recipeName.innerText.includes(searchRecipesInput.value) ||
+          recipeIngredient.innerText.includes(searchRecipesInput.value) ||
+          recipeTag.innerText.includes(searchRecipesInput.value)) {
+          recipeCards[i].classList.remove('hidden');
+        } else {
+          recipeCards[i].classList.add('hidden');
+        }
+      }
+  //   if (!recipeName.innerText.includes(searchRecipesInput.value) ||
+  //     !recipeIngredient.innerText.includes(searchRecipesInput.value) ||
+  //     !recipeTag.innerText.includes(searchRecipesInput.value)) {
+  //     recipeCards[i].classList.add('hidden');
+  //   } else {
+  //     recipeCards[i].classList.remove('hidden');
+  //   }
+  // }
 }
 
-
+function searchPantry() {
+  var ingredientCards = Array.from(document.querySelectorAll('.ingedient-card'));
+  for (var i = 0; i < ingredientCards.length; i++) {
+    var ingredientName = ingredientCards[i].querySelector('.ingredient- name');
+    if (!ingredientName.innerText.includes(searchPantryInput.value)) {
+      ingredientCards[i].classList.add('hidden');
+    } else {
+      ingredientCards[i].classList.remove('hidden');
+    }
+  }
+}
 
 function convertRecipeIdsToIngredientNames(recipe) {
   recipe.ingredients.forEach(ingredient => {
@@ -176,7 +180,7 @@ function displayRecipes() {
           <button class="recipe-button add-ingredients-button">Add Ingredients to Shopping List</button>
           <button class="recipe-button add-to-queue-button">Add Recipe to Cooking Queue</button>
         </div>
-        <p class="capitalize">Tags: ${newRecipe.tags}</p>
+        <p class="capitalize recipe-tags">Tags: ${newRecipe.tags}</p>
       </div>
     </section>`
   });
